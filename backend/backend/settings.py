ddb16b60501b8cb14d86b83daacc63a555238657
Myat Inzally Khine <myatinzallykhine@gmail.com>
2025-02-27 20:11:16 -0800

    frontend separation

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,11 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'campgroundapi',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -68,6 +72,20 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK= {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ]
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
@@ -123,3 +141,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
--- backend/campgroundapi/urls.py
+++ backend/campgroundapi/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path,include
 from .views import CreateCampView,CreateCommentView,CreateUserView,DeleteCampView,DeleteCommentView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
-    path('campground/create', CreateCampView.as_view(), name='campground-list'),
-    path('user/register', CreateUserView.as_view(), name='register'),
-    path('campground/delete/<int:pk>', DeleteCampView.as_view(), name='delete-campground'),
-    path('comments', CreateCommentView.as_view(), name='comments'),
-    path('comment/delete/<int:pk>', DeleteCommentView.as_view(), name='delete-comment'),
+    path('campground/create/', CreateCampView.as_view(), name='campground-list'),
+    path('user/register/', CreateUserView.as_view(), name='register'),
+    path('campground/delete/<int:pk>/', DeleteCampView.as_view(), name='delete-campground'),
+    path('comments/', CreateCommentView.as_view(), name='comments'),
+    path('comment/delete/<int:pk>/', DeleteCommentView.as_view(), name='delete-comment'),
+    path('token/',TokenObtainPairView.as_view(),name='get_token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('campgroundapi-auth', include("rest_framework.urls")),
 
 ]
